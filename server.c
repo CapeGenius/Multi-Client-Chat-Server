c48ac59176c50765d6595854ed0dc91ef8a6bb54
Rohan Bendapudi <datanteater@gmail.com>
2025-11-01 23:31:13 -0400

    trying to get the server to echo back the information sent

--- server.c
+++ server.c
@@ -5,11 +5,13 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#define BUFFER_SIZE 1024
 
 int main() {
     int reuse = 1;
     int listener_d = socket(AF_INET, SOCK_STREAM, 0);
-    char* msg = "Hello! I'm hungry.";
+    int connection_size = 10;
 
     // checking if the file descriptor is created
     if (listener_d < 0){
@@ -17,11 +19,11 @@ int main() {
         exit(1);
     }
 
-    printf("Socket created");
+    printf("Socket created \n");
 
     if (setsockopt(listener_d, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(int)) == -1)
     {
-        perror("Can't set the reuse option on the socket");
+        perror("Can't set the reuse option on the socket\n");
     }
 
     // declaring a struct for the IP address
@@ -31,34 +33,96 @@ int main() {
     servAddr.sin_addr.s_addr = INADDR_ANY;
 
     // ensuring that the socket binds to port
-    if (bind(listener_d, (struct sockaddr_in*)&servAddr, sizeof(servAddr)) < 0) {
-        perror("Failed to bind to port");
+    if (bind(listener_d, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
+        perror("Failed to bind to port \n");
         exit(1);
     }
 
-    printf("Socket bound to port");
+    printf("Socket bound to port \n");
 
     //ensure that socket listens to new connections
-    if (listen(listener_d, 1) < 0) {
-        perror("Couldn't listen on socket");
+    if (listen(listener_d, connection_size) < 0) {
+        perror("Couldn't listen on socket \n");
     }
 
-    printf("Socket listening on port");
+    printf("Socket listening on port \n");
 
-    int clientSocket = accept(listener_d, NULL, NULL);
-    if (clientSocket < 0) {
-        perror("Client Connection Faileed");
-        exit(1);
+    while (1)
+    {
+        int clientSocket = accept(listener_d, NULL, NULL);
+
+        if (clientSocket < 0) {
+            perror("Client Connection Faileed \n");
+            exit(1);
+        }
+
+        pid_t child_process = fork();
+
+        if (child_process == 0) {
+            close(listener_d);
+
+            client_handling(clientSocket);
+            exit(0);
+        } 
+        else
+            close(clientSocket);
     }
 
-    printf("Client connected to server");
-    send(clientSocket, msg, strlen(msg), 0);
-
-    close(clientSocket);
     close(listener_d);
 
     return 0;
 
-    
+}
 
+int send_info(int socket, char* msg){
+    // dereferences client socket and sends message to socket
+    char new_line[] = "\n";
+    char server_msg[] = "Server:";
+
+    char* temp_msg = strcat(msg, new_line);
+    char* final_msg = strcat(server_msg, temp_msg);
+
+    int success = send(socket, final_msg, strlen(msg), 0);
+    if (success == -1) {
+        perror("Send failed");
+    }
+
+    return success;
+}
+
+int read_info(int socket, char *buf, int len){
+    char *s = buf;
+    int slen = len;
+    int c = recv(socket, s, slen, 0);
+
+    while ((c > 0) && (s[c-1] != '\n')) {
+        s += c; slen -= c;
+        c = recv(socket, s, slen, 0);
+    }
+    if (c < 0)
+        return c;
+    else if (c == 0) {
+        buf[0] = '\0';
+        return 0;
+    }
+    else
+        s[c -1] = '\0';
+    return len - slen;
+}
+
+void client_handling(int clientSocket) {
+    char buffer[BUFFER_SIZE]; 
+    while(1) {
+        int byte_count = read_info(clientSocket, buffer, BUFFER_SIZE);
+
+        if (strcmp(buffer, "exit") == 0) {
+            printf("Client requested exit.\n");
+            break;
+        }
+
+        send_info(clientSocket, buffer);
+        
+    }
+
+    close(clientSocket);
 }
