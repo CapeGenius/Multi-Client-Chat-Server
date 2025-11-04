b80ce81de8397ddb2289d09c88e6d4c804a32bab
Rohan Bendapudi <datanteater@gmail.com>
2025-11-04 11:40:09 -0500

    added necessary libraries for client.c

---
+++ client.c
@@ -0,0 +1,9 @@
+#include <sys/socket.h>   
+#include <netinet/in.h>   
+#include <arpa/inet.h>    
+#include <pthread.h>      
+#include <stdio.h>        
+#include <stdlib.h>       
+#include <string.h>       
+#include <unistd.h>
+#include <time.h> 
