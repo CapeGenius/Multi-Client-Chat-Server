51507503bc2fef7d59e95fdfd98676f203900c3a
Rohan Bendapudi <datanteater@gmail.com>
2025-11-06 12:19:47 -0500

    created a client main file to create a client executable

---
+++ client.c
@@ -0,0 +1,28 @@
+#include "server_setup.h"
+#include <stdio.h>
+
+int main() {
+    char ip[16];
+    prompt_ip(ip, sizeof(ip));
+    int port = prompt_port();
+
+    int client_socket = connect_client(ip, &port);
+
+    close(client_socket);
+
+    return 0;
+}
+
+
+void prompt_ip(char* buffer, size_t size) {
+    puts("Type Remote IP Address:");
+    scanf("%15s", buffer);
+}
+
+int prompt_port() {
+    int port;
+    puts("Type Port:");
+    scanf("%d", port);
+
+    return port;
+}
